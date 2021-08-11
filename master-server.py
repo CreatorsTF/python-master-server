189521beb35d44be4fe013e96e98abab30a0d408
steph <sappho@sappho.io>
2021-08-11 00:58:34 -0400

    update

--- master-server.py
+++ master-server.py
@@ -1,6 +1,21 @@
 import a2s
 import requests
 import json
+import time
+import socket
+from colorama import Fore
+import sys
+import signal
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+#webhook = DiscordWebhook(url='https://canary.discord.com/api/webhooks/874855141075984435/q_YE9jAoQN8fwdOh9n2a4cLg8sCXfPSllEK8a6wd-3sD0goyfJesTaJDgsYMF2_Zo_M9')
+#
+## create embed object for webhook
+## you can set the color as a decimal (color=242424) or hex (color='03b2f8') number
+#embed = DiscordEmbed(title='Python Master Server', description='{}', color='00ffbf')
+#embed.add_embed_field(name='{}', value='{}')
+#webhook.add_embed(embed)
+#response = webhook.execute()
 
 # Creators.TF Master Server
 # Written by ZoNiCaL.
@@ -12,8 +27,12 @@ import json
 # hardcode them until we decide to flesh out providers more.
 providers = [ 15, 1756, 11919, 78132 ]
 
+# time to sleep after running thru all providers
+sleeptime = 10;
+
 # Custom exception class to diagnose problems that specifically happen with the
 # Creators.TF API. This could help diagnose problems later if needed.
+# ^^^ ??? what??
 class CreatorsTFAPIError(BaseException):
     pass
 
@@ -28,81 +47,105 @@ except Exception as e:
     print(f"Failed to load config and grab API key: {e}")
     quit()
 
-# Grab our server list with an HTTP request.
-for provider in providers:
-    requestURL = f"https://creators.tf/api/IServers/GServerList?provider={provider}"
-    try: # Make an API request to the website.
-        req = requests.get(requestURL, timeout=10)    # Return a JSON object which we can iterate over.
-        serverList = req.json()
-
-        # If the API returned something that wasn't a SUCCESS, something wen't wrong
-        # on the website-end, so we'll just skip this provider for now.
-        if serverList["result"] != "SUCCESS":
-            raise CreatorsTFAPIError(f"[{req.status_code}] API (GServerList) returned non-SUCCESS code.")
-
-    # If we run into some sort of error making a request, we'll just skip this provider.
-    except BaseException as e:
-        print(f"[FAIL] Server {server['ip']}:{server['port']} failed to respond to A2S: {e}")
-        continue
-
-    serversToSend = []
-
-    # Loop through all the returned servers and request information for them.
-    for server in serverList["servers"]:
+while True:
+    # Grab our server list with an HTTP request.
+    for provider in providers:
+        requestURL = f"https://creators.tf/api/IServers/GServerList?provider={provider}"
+        # Make an API request to the website.
         try:
-            serverID = server["id"]
-            a2sInfoRequest = a2s.info((server["ip"], server["port"]))
+            req = requests.get(requestURL, timeout=5)    # Return a JSON object which we can iterate over.
+            serverList = req.json()
 
-            # Construct a JSON object with all of our server information.
-            info = {
-                "hostname": a2sInfoRequest.server_name,
-                "online": a2sInfoRequest.player_count,
-                "maxplayers": a2sInfoRequest.max_players,
-                "map": a2sInfoRequest.map_name,
-                "keywords": a2sInfoRequest.keywords,
-                "bots": a2sInfoRequest.bot_count,
-                "game": a2sInfoRequest.game,
-                "appid": a2sInfoRequest.app_id,
-                "version": a2sInfoRequest.version,
-                "passworded": a2sInfoRequest.password_protected,
-                "vac_secure": a2sInfoRequest.vac_enabled,
-                "sourcetv_port": a2sInfoRequest.stv_port,
-                "sourcetv_name": a2sInfoRequest.stv_name,
-            }
+            #print(f"{serverList}")
 
-            # This could totally have more support for more data later like
-            # actual players. If we consider doing a "recent activity" feature,
-            # we could return other player info.
+            # If the API returned something that wasn't a SUCCESS, something wen't wrong
+            # on the website-end, so we'll just skip this provider for now.
+            if serverList["result"] != "SUCCESS":
+                raise CreatorsTFAPIError(f"[{req.status_code}] API (GServerList) returned non-SUCCESS code.")
 
-            #Construct an object that we'll send to the database soon:
-            serverToSend = {
-                "id": serverID,
-                "datapack": info
-            }
+        # If we run into some sort of error making a request, we'll just skip this provider.
+        except BaseException as e:
+            print(f"[FAIL] Request failed for provider {provider}: {e}")
+            continue
 
-            # Add to our final list:
-            serversToSend.append(serverToSend)
-            print(f"[SEARCH] Server {server['ip']}:{server['port']}: {a2sInfoRequest.server_name}, {a2sInfoRequest.player_count}/{a2sInfoRequest.max_players}")
+        serversToSend = []
 
-        except BaseException as e: 
-            # We ran into some sort of error here when making a request.
-            # We'll skip over this server for now and just log the error.
-            print(f"[FAIL] Server {server['ip']}:{server['port']} failed to respond to A2S: {e}")
+        # Loop through all the returned servers and request information for them.
+        for server in serverList["servers"]:
+            serverstr = str(server['ip']) + ":" + str(server['port'])
+            try:
+                serverID = server["id"]
+                timeout = 2.0
+                a2sInfoRequest = a2s.info((server["ip"], server["port"]), timeout)
 
-    # We've now got a list of servers for this provider. Create a request to the
-    # website API that updates server information in the database.
-    requestURL = f"https://creators.tf/api/IServers/GHeartbeat"
-    try: # Make an API request to the website.
-        req = requests.post(requestURL,
-            data={ "servers": serversToSend, "key": masterKey })
-        resp = req.json()   # Return a JSON object which we can iterate over.
+                # Construct a JSON object with all of our server information.
+                info = {
+                    "hostname":         a2sInfoRequest.server_name,
+                    "online":           a2sInfoRequest.player_count,
+                    "maxplayers":       a2sInfoRequest.max_players,
+                    "map":              a2sInfoRequest.map_name,
+                    "keywords":         a2sInfoRequest.keywords,
+                    "bots":             a2sInfoRequest.bot_count,
+                    "game":             a2sInfoRequest.game,
+                    "appid":            a2sInfoRequest.app_id,
+                    "version":          a2sInfoRequest.version,
+                    "passworded":       a2sInfoRequest.password_protected,
+                    "vac_secure":       a2sInfoRequest.vac_enabled,
+                    "sourcetv_port":    a2sInfoRequest.stv_port,
+                    "sourcetv_name":    a2sInfoRequest.stv_name,
+                }
 
-        # If the API returned something that wasn't a SUCCESS, something wen't wrong
-        # on the website-end, so we'll just skip this provider for now.
-        if resp["result"] != "SUCCESS":
-            raise CreatorsTFAPIError(f"[{req.status_code}] API (GHeartbeat) returned non-SUCCESS code.")
+                # This could totally have more support for more data later like
+                # actual players. If we consider doing a "recent activity" feature,
+                # we could return other player info.
 
-        print(f"Successfully updated provider {provider}")
-    except BaseException as e:
-        print(e)
-        continue
+                #Construct an object that we'll send to the database soon:
+                serverToSend = {
+                    "id": serverID,
+                    "datapack": info
+                }
+
+                # Add to our final list:
+                serversToSend.append(serverToSend)
+
+                print(Fore.GREEN + f"[SUCCESS] {serverstr}: {a2sInfoRequest.server_name}, {a2sInfoRequest.player_count}/{a2sInfoRequest.max_players}" + Fore.RESET)
+
+            except socket.timeout:
+                print(Fore.RED + f"[TIMEOUT] {serverstr}" + Fore.RESET)
+            except ConnectionRefusedError:
+                print(Fore.RED + f"[REFUSED] {serverstr}" + Fore.RESET)
+            except socket.gaierror:
+                print(Fore.RED + f"[NOSERVER] {serverstr}" + Fore.RESET)
+            except OSError:
+                print(Fore.RED + f"[OSERROR] {serverstr}" + Fore.RESET)
+
+        # We've now got a list of servers for this provider. Create a request to the
+        # website API that updates server information in the database.
+        requestURL = f"https://creators.tf/api/IServers/GHeartbeat"
+        # Make an API request to the website.
+        try:
+            req = requests.post(requestURL,
+                data={ "servers": serversToSend, "key": masterKey })
+            resp = req.json()   # Return a JSON object which we can iterate over.
+
+            # If the API returned something that wasn't a SUCCESS, something wen't wrong
+            # on the website-end, so we'll just skip this provider for now.
+            if resp["result"] != "SUCCESS":
+                raise CreatorsTFAPIError(f"[{req.status_code}] API (GHeartbeat) returned non-SUCCESS code.")
+
+            print(f"Successfully updated provider {provider}")
+        except BaseException as e:
+            print(e)
+            continue
+
+    # stupid bad code
+    seconds = "";
+    if sleeptime != 1:
+        seconds = "s"
+
+    print(Fore.MAGENTA + f"Sleeping for {int(sleeptime)} second{seconds}..."  + Fore.RESET)
+    time.sleep(int(sleeptime));
+
+
+os._exit(1)
+
