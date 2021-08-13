a191851210341b32ec221de586227546b2857db8
zonical1 <zonicalguy@gmail.com>
2021-08-13 17:39:26 +1200

    new code

--- master-server.py
+++ master-server.py
@@ -67,111 +67,135 @@ except Exception as e:
     print(f"Failed to load config and grab API key: {e}")
     quit()
 
-while True:
-    # Grab our server list with an HTTP request.
-    for provider in providers:
-        requestURL = f"https://creators.tf/api/IServers/GServerList?provider={provider}"
-        # Make an API request to the website.
-        try:
-            req = requests.get(requestURL, timeout=5)    # Return a JSON object which we can iterate over.
-            serverList = req.json()
+def GrabServersForProvider(providerID):
+    requestURL = f"https://creators.tf/api/IServers/GServerList?provider={providerID}"
+    # Make an API request to the website.
+    try:
+        req = requests.get(requestURL, timeout=5)    # Return a JSON object which we can iterate over.
+        serverList = req.json()
 
-            #print(f"{serverList}")
+        # If the API returned something that wasn't a SUCCESS, something wen't wrong
+        # on the website-end, so we'll just skip this provider for now.
+        if serverList["result"] != "SUCCESS":
+            raise CreatorsTFAPIError(f"[{req.status_code}] API (GServerList) returned non-SUCCESS code.")
 
-            # If the API returned something that wasn't a SUCCESS, something wen't wrong
-            # on the website-end, so we'll just skip this provider for now.
-            if serverList["result"] != "SUCCESS":
-                raise CreatorsTFAPIError(f"[{req.status_code}] API (GServerList) returned non-SUCCESS code.")
+    # If we run into some sort of error making a request, we'll just skip this provider.
+    except BaseException as e:
+        print(f"[FAIL] Request failed for provider {providerID}: {e}")
+    
+    return serverList["servers"]
 
-        # If we run into some sort of error making a request, we'll just skip this provider.
-        except BaseException as e:
-            print(f"[FAIL] Request failed for provider {provider}: {e}")
-            continue
+# This function queries our server with A2S and returns a dict with all of the server information.
+def QueryServer(serverID, serverInfo):
+    try:
+        timeout = 3
+        a2sInfoRequest = a2s.info(serverInfo, timeout)
 
-        serversToSend = []
+        # Construct a JSON object with all of our server information.
+        info = {
+            "name":             a2sInfoRequest.server_name,
+            "players":           a2sInfoRequest.player_count,
+            "maxplayers":       a2sInfoRequest.max_players,
+            "map":              a2sInfoRequest.map_name,
+            "keywords":         a2sInfoRequest.keywords,
+            "bots":             a2sInfoRequest.bot_count,
+            "game":             a2sInfoRequest.game,
+            "appid":            a2sInfoRequest.app_id,
+            "version":          a2sInfoRequest.version,
+            "passworded":       a2sInfoRequest.password_protected,
+            "vac_secure":       a2sInfoRequest.vac_enabled,
+            "sourcetv_port":    a2sInfoRequest.stv_port,
+            "sourcetv_name":    a2sInfoRequest.stv_name,
+        }
 
-        # Loop through all the returned servers and request information for them.
-        for server in serverList["servers"]:
-            # sleep so we dont spam servers
-            time.sleep(1)
-            print(server["ip"], server["port"])
-            serverstr = str(server['ip']) + ":" + str(server['port'])
-            try:
-                serverID = server["id"]
-                timeout = 10.0
-                a2sInfoRequest = a2s.info((server["ip"], server["port"]), timeout)
+        # This could totally have more support for more data later like
+        # actual players. If we consider doing a "recent activity" feature,
+        # we could return other player info.
 
-                # Construct a JSON object with all of our server information.
-                info = {
-                    "hostname":         a2sInfoRequest.server_name,
-                    "online":           a2sInfoRequest.player_count,
-                    "maxplayers":       a2sInfoRequest.max_players,
-                    "map":              a2sInfoRequest.map_name
-                    #"keywords":         a2sInfoRequest.keywords,
-                    #"bots":             a2sInfoRequest.bot_count,
-                    #"game":             a2sInfoRequest.game,
-                    #"appid":            a2sInfoRequest.app_id,
-                    #"version":          a2sInfoRequest.version,
-                    #"passworded":       a2sInfoRequest.password_protected,
-                    #"vac_secure":       a2sInfoRequest.vac_enabled,
-                    #"sourcetv_port":    a2sInfoRequest.stv_port,
-                    #"sourcetv_name":    a2sInfoRequest.stv_name,
-                }
+        #Construct an object that we'll send to the database soon:
+        serverToSend = {
+            "id": serverID,
+            "datapack": {
+                "info": info
+            }
+        }
 
-                # This could totally have more support for more data later like
-                # actual players. If we consider doing a "recent activity" feature,
-                # we could return other player info.
+        # Woo! Success! Log it.
+        print(Fore.GREEN + f"[SUCCESS] {serverInfo}: {a2sInfoRequest.server_name}, {a2sInfoRequest.player_count}/{a2sInfoRequest.max_players}" + Fore.RESET)
+        return serverToSend
 
-                #Construct an object that we'll send to the database soon:
-                serverToSend = {
-                    "id": serverID,
-                    "datapack": info
-                }
+    except socket.timeout:
+        print(Fore.RED + f"[TIMEOUT] {serverInfo}" + Fore.RESET)
+    except ConnectionRefusedError:
+        print(Fore.RED + f"[REFUSED] {serverInfo}" + Fore.RESET)
+    except socket.gaierror:
+        print(Fore.RED + f"[NOSERVER] {serverInfo}" + Fore.RESET)
+    except OSError:
+        print(Fore.RED + f"[OSERROR] {serverInfo}" + Fore.RESET)
 
-                # Add to our final list:
-                serversToSend.append(serverToSend)
+def SendServersToHeartbeat(servers):
+    print(Fore.YELLOW + f"[PENDING] Sending block of {len(servers)} servers to api/IServers/GHeartbeat..."  + Fore.RESET)
 
-                print(Fore.GREEN + f"[SUCCESS] {serverstr}: {a2sInfoRequest.server_name}, {a2sInfoRequest.player_count}/{a2sInfoRequest.max_players}" + Fore.RESET)
+    # We've now got a list of servers to send. Create a request to the
+    # website API that updates server information in the database.
+    requestURL = f"https://creators.tf/api/IServers/GHeartbeat"
+    # Make an API request to the website.
+    try:
+        req = requests.post(requestURL,
+            data={ "servers": json.dumps(servers), "key": masterKey })
+        resp = req.json()   # Return a JSON object which we can iterate over.
+        print(resp)
 
-            #except BrokenMessageError:
-            #    print("a")
-            except socket.timeout:
-                print(Fore.RED + f"[TIMEOUT] {serverstr}" + Fore.RESET)
-            except ConnectionRefusedError:
-                print(Fore.RED + f"[REFUSED] {serverstr}" + Fore.RESET)
-            except socket.gaierror:
-                print(Fore.RED + f"[NOSERVER] {serverstr}" + Fore.RESET)
-            except OSError:
-                print(Fore.RED + f"[OSERROR] {serverstr}" + Fore.RESET)
+        # If the API returned something that wasn't a SUCCESS, something wen't wrong
+        # on the website-end, so we'll just skip this provider for now.
+        if resp["result"] != "SUCCESS":
+            raise CreatorsTFAPIError(f"[{req.status_code}] API (GHeartbeat) returned non-SUCCESS code.")
 
-        # We've now got a list of servers for this provider. Create a request to the
-        # website API that updates server information in the database.
-        requestURL = f"https://creators.tf/api/IServers/GHeartbeat"
-        # Make an API request to the website.
-        try:
-            req = requests.post(requestURL,
-                data={ "servers": serversToSend, "key": masterKey })
-            resp = req.json()   # Return a JSON object which we can iterate over.
+    except BaseException as e:
+        print(e)
 
-            # If the API returned something that wasn't a SUCCESS, something wen't wrong
-            # on the website-end, so we'll just skip this provider for now.
-            if resp["result"] != "SUCCESS":
-                raise CreatorsTFAPIError(f"[{req.status_code}] API (GHeartbeat) returned non-SUCCESS code.")
+    print(Fore.GREEN + f"[SUCCESS] Sent! Block of {len(servers)} servers arrived to api/IServers/GHeartbeat..."  + Fore.RESET)
 
-            print(f"Successfully updated provider {provider}")
-        except BaseException as e:
-            print(e)
-            continue
+def MasterServer():
+    while True:
+        # Grab our server list with an HTTP request.
+        for provider in providers: 
+            servers = GrabServersForProvider(provider)
 
-    # stupid bad code awful dogshit don't do this
-    seconds = "";
-    if sleeptime != 1:
-        seconds = "s"
+            # We now have a list of servers. We're going to create blocks where we'll query
+            # a block of servers and ship them off in a request. By default, we'll have
+            # five servers in a block that we'll send off in a list. If we happen to have
+            # less than five servers in a block because we've reached the end of a list for
+            # a provider, that's alright, we'll send them anyways.
+            serverBlock = []
 
-    print(Fore.MAGENTA + f"Sleeping for {int(sleeptime)} second{seconds}..."  + Fore.RESET)
-    time.sleep(int(sleeptime));
+            for server in servers:
+                # Take a second to breathe so we don't spam our servers too much.
+                time.sleep(2.0)
 
+                # Okay, now lets send a query to the server asking for information.
+                result = QueryServer(server["id"], (server["ip"], server["port"]))
 
-os._exit(1)
+                # Do we have a block of five servers we can ship off?
+                if (len(serverBlock) <= 0): # No? Append the list.
+                    if result != None:
+                        serverBlock.append(result)
+                else: # We already have a block of five servers, ship it off.
+                    SendServersToHeartbeat(serverBlock)
+                    serverBlock.clear()
+
+                    # Add this server to the list afterwards as well.
+                    if result != None:
+                        serverBlock.append(result)
+
+            # If we have any servers remaining in our server block, just send them over.
+            if (len(serverBlock) != 0):
+                SendServersToHeartbeat(serverBlock)
+
+        print(Fore.MAGENTA + f"Sleeping for {int(sleeptime)} seconds..."  + Fore.RESET)
+        time.sleep(int(sleeptime))
+
+if (__name__ == "__main__"):
+    MasterServer()
 
 #TODO ctrl c handling
